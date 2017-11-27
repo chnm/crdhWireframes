b5dbd91914342bd9f32a298f305137144833a0d7
Ken Albers <ken.albers@gmail.com>
2017-11-27 15:23:22 -0500

    link fix

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
             <h4><a href="essay.php">Scintillating Title: With Sober Subtitle of a Certain Length</a></h4>
             <p class="author">Jane Q Academic</p>
             <!-- <p class="short-description">A short description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur, optio cum eveniet ab unde ipsum vero minus reiciendis non qui nam iusto quod odio repellat iure reprehenderit. Illo, illum!</p> -->
-            <a data-toggle="abstract">Abstract Link</a>
+            <a data-toggle="abstract">Abstract</a>
             <p id="abstract" data-toggler data-animate="slide-in-down slide-out-up">The longer abstract. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis voluptatem itaque est harum error, commodi impedit eaque minus accusamus veritatis, beatae. Fuga perspiciatis optio, iure quae quis illum obcaecati recusandae!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, consequatur nam, assumenda voluptatum officiis velit saepe repellat molestiae, voluptatem vel facilis aperiam omnis! Esse architecto necessitatibus quo, nemo, fuga labore?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi dolore sapiente, commodi explicabo nobis consectetur consequuntur ipsa corrupti, beatae qui quisquam perspiciatis eum quas nihil vero distinctio, molestiae aut accusantium!</p>
           </div>
 
@@ -17,7 +17,7 @@
             <h4>Scintillating Title: With Sober Subtitle of a Certain Length</h4>
             <p class="author">Jane Q Academic</p>
             <!-- <p class="short-description">A short description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur, optio cum eveniet ab unde ipsum vero minus reiciendis non qui nam iusto quod odio repellat iure reprehenderit. Illo, illum!</p> -->
-            <a data-toggle="abstract">Abstract Link</a>
+            <a data-toggle="abstract">Abstract</a>
             <p id="abstract" data-toggler data-animate="slide-in-down slide-out-up">The longer abstract. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis voluptatem itaque est harum error, commodi impedit eaque minus accusamus veritatis, beatae. Fuga perspiciatis optio, iure quae quis illum obcaecati recusandae!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, consequatur nam, assumenda voluptatum officiis velit saepe repellat molestiae, voluptatem vel facilis aperiam omnis! Esse architecto necessitatibus quo, nemo, fuga labore?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi dolore sapiente, commodi explicabo nobis consectetur consequuntur ipsa corrupti, beatae qui quisquam perspiciatis eum quas nihil vero distinctio, molestiae aut accusantium!</p>
           </div>
 
@@ -26,7 +26,7 @@
             <h4>Maybe a Blander Title: With a Subtitle of an Unnecessarily Longer Length Because the Author Hates Conciseness</h4>
             <p class="author">Jane Q Academic</p>
             <!-- <p class="short-description">A short description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur, optio cum eveniet ab unde ipsum vero minus reiciendis non qui nam iusto quod odio repellat iure reprehenderit. Illo, illum!</p> -->
-            <a data-toggle="abstract">Abstract Link</a>
+            <a data-toggle="abstract">Abstract</a>
             <p id="abstract" data-toggler data-animate="slide-in-down slide-out-up">The longer abstract. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis voluptatem itaque est harum error, commodi impedit eaque minus accusamus veritatis, beatae. Fuga perspiciatis optio, iure quae quis illum obcaecati recusandae!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, consequatur nam, assumenda voluptatum officiis velit saepe repellat molestiae, voluptatem vel facilis aperiam omnis! Esse architecto necessitatibus quo, nemo, fuga labore?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi dolore sapiente, commodi explicabo nobis consectetur consequuntur ipsa corrupti, beatae qui quisquam perspiciatis eum quas nihil vero distinctio, molestiae aut accusantium!</p>
           </div>
 
@@ -35,7 +35,7 @@
             <h4>Scintillating Title: With Sober Subtitle of a Certain Length</h4>
             <p class="author">Jane Q Academic</p>
             <!-- <p class="short-description">A short description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur, optio cum eveniet ab unde ipsum vero minus reiciendis non qui nam iusto quod odio repellat iure reprehenderit. Illo, illum!</p> -->
-            <a data-toggle="abstract">Abstract Link</a>
+            <a data-toggle="abstract">Abstract</a>
             <p id="abstract" data-toggler data-animate="slide-in-down slide-out-up">The longer abstract. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis voluptatem itaque est harum error, commodi impedit eaque minus accusamus veritatis, beatae. Fuga perspiciatis optio, iure quae quis illum obcaecati recusandae!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, consequatur nam, assumenda voluptatum officiis velit saepe repellat molestiae, voluptatem vel facilis aperiam omnis! Esse architecto necessitatibus quo, nemo, fuga labore?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi dolore sapiente, commodi explicabo nobis consectetur consequuntur ipsa corrupti, beatae qui quisquam perspiciatis eum quas nihil vero distinctio, molestiae aut accusantium!</p>
           </div>
 
@@ -44,7 +44,7 @@
             <h4>Scintillating Title: With Sober Subtitle of a Certain Length</h4>
             <p class="author">Jane Q Academic</p>
             <!-- <p class="short-description">A short description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur, optio cum eveniet ab unde ipsum vero minus reiciendis non qui nam iusto quod odio repellat iure reprehenderit. Illo, illum!</p> -->
-            <a data-toggle="abstract">Abstract Link</a>
+            <a data-toggle="abstract">Abstract</a>
             <p id="abstract" data-toggler data-animate="slide-in-down slide-out-up">The longer abstract. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis voluptatem itaque est harum error, commodi impedit eaque minus accusamus veritatis, beatae. Fuga perspiciatis optio, iure quae quis illum obcaecati recusandae!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, consequatur nam, assumenda voluptatum officiis velit saepe repellat molestiae, voluptatem vel facilis aperiam omnis! Esse architecto necessitatibus quo, nemo, fuga labore?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi dolore sapiente, commodi explicabo nobis consectetur consequuntur ipsa corrupti, beatae qui quisquam perspiciatis eum quas nihil vero distinctio, molestiae aut accusantium!</p>
           </div>
 
@@ -53,7 +53,7 @@
             <h4>Scintillating Title: With Sober Subtitle of a Certain Length</h4>
             <p class="author">Jane Q Academic</p>
             <!-- <p class="short-description">A short description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur, optio cum eveniet ab unde ipsum vero minus reiciendis non qui nam iusto quod odio repellat iure reprehenderit. Illo, illum!</p> -->
-            <a data-toggle="abstract">Abstract Link</a>
+            <a data-toggle="abstract">Abstract</a>
             <p id="abstract" data-toggler data-animate="slide-in-down slide-out-up">The longer abstract. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis voluptatem itaque est harum error, commodi impedit eaque minus accusamus veritatis, beatae. Fuga perspiciatis optio, iure quae quis illum obcaecati recusandae!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, consequatur nam, assumenda voluptatum officiis velit saepe repellat molestiae, voluptatem vel facilis aperiam omnis! Esse architecto necessitatibus quo, nemo, fuga labore?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi dolore sapiente, commodi explicabo nobis consectetur consequuntur ipsa corrupti, beatae qui quisquam perspiciatis eum quas nihil vero distinctio, molestiae aut accusantium!</p>
           </div>
 
@@ -62,7 +62,7 @@
             <h4>Scintillating Title: With Sober Subtitle of a Certain Length</h4>
             <p class="author">Jane Q Academic</p>
             <!-- <p class="short-description">A short description. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur, optio cum eveniet ab unde ipsum vero minus reiciendis non qui nam iusto quod odio repellat iure reprehenderit. Illo, illum!</p> -->
-            <a data-toggle="abstract">Abstract Link</a>
+            <a data-toggle="abstract">Abstract</a>
             <p id="abstract" data-toggler data-animate="slide-in-down slide-out-up">The longer abstract. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis voluptatem itaque est harum error, commodi impedit eaque minus accusamus veritatis, beatae. Fuga perspiciatis optio, iure quae quis illum obcaecati recusandae!Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, consequatur nam, assumenda voluptatum officiis velit saepe repellat molestiae, voluptatem vel facilis aperiam omnis! Esse architecto necessitatibus quo, nemo, fuga labore?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi dolore sapiente, commodi explicabo nobis consectetur consequuntur ipsa corrupti, beatae qui quisquam perspiciatis eum quas nihil vero distinctio, molestiae aut accusantium!</p>
           </div>
 
